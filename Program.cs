9d0493470b97881786283018f5a15910707249a6
weisere <112728696+weisere@users.noreply.github.com>
2023-10-25 19:38:15 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string LinkedListToString(LinkedList<string> ll, int place)
+{
+    if (ll == null || ll.Count == 0)
+    {
+        return "";
+    }
+
+    if (place >= ll.Count)
+    {
+        return ""; 
+    }
+
+
+    LinkedListNode<string> node = ll.First;
+    for (int i = 0; i < place; i++)
+    {
+        node = node.Next;
+    }
+
+    Console.WriteLine(node.Value);
+
+    return LinkedListToString(ll, place+1);
+}
+
+LinkedList<string> list = new LinkedList<string>();
+list.AddLast("Apple");
+list.AddLast("Banana");
+list.AddLast("Cherry");
+list.AddLast("Date");
+
+LinkedListToString(list, 0);
